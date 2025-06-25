8752182dc51b1942fe076410e37bbcb05415cec4
LittleOTwo <152656767+LittleOTwo@users.noreply.github.com>
2025-06-25 15:12:45 -0300

    Update rtc_e_sol.h
    rtc_e_sol.h atualizado pra condizer com a mudança em rtc.c

--- rtc_e_sol.h
+++ rtc_e_sol.h
@@ -1,6 +1,7 @@
 #ifndef RTC_H_
 #define RTC_H_
 #include <stdint.h>
+#include <device.h>
 
 // Definir um novo tipo de função chamado data_t, que agrupa todos os valores relevantes
 // em uma variável só.
@@ -20,10 +21,8 @@ extern const double longitude_local_graus; // São Paulo (negativo para Oeste)
 extern const double longitude_meridiano_padrao_graus; // Para UTC-3 (BRT)
 
 // Funções fornecidas pela biblioteca
-void I2C0_Init();
-void RTC_Verificar_E_Configurar_Se_Necessario(data_t *valores_calibracao_padrao);
-void configurar_RTC(data_t *calibracao);
-void ler_RTC(data_t *agora);
+void RTC_Setup(const struct device *i2c_dev, const data_t *calibracao);
+void RTC_Ler(const struct device *i2c_dev);
 void delay_ms(volatile uint32_t ms);
 
 double angulo_horario_solar(void);
